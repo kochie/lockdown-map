ca7e1b8451468b650733f5ab359a8e37e9545be1
Robert Koch <robert@kochie.io>
2020-07-04 14:02:34 +1000

    map displays filtered postcodes

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -2,37 +2,105 @@ import React, { useRef, useEffect } from 'react'
 import mapboxgl from 'mapbox-gl'
 import Head from 'next/head'
 
-import styles from "../styles/map.module.css"
+import styles from '../styles/map.module.css'
 
-mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN;
+mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN
 
 const Map = (): JSX.Element => {
-    const mapRef = useRef<HTMLDivElement>(null)
- 
-    useEffect(() => {
-        const map = new mapboxgl.Map({
-            container: mapRef.current,
-            style: 'mapbox://styles/mapbox/streets-v11',
-            center: {lat: -36.5766575, lng: 144.3685087},
-            zoom: 8
-            // zoom: 13,
-            // center: [4.899, 52.372]
-        });
+  const mapRef = useRef<HTMLDivElement>(null)
+  const postcodeId = 'kochie.cjhqarkd'
+  const restrictedPostcodes = [
+    '3012',
+    '3021',
+    '3032',
+    '3038',
+    '3042',
+    '3046',
+    '3047',
+    '3055',
+    '3060',
+    '3064',
+  ]
 
-        return () => {
-            map.remove()
-        }
-    }, []) 
+  useEffect(() => {
+    const map = new mapboxgl.Map({
+      container: mapRef.current,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: { lat: -37.7423107, lng: 145.0127503 },
+      zoom: 10,
+      // zoom: 13,
+      // center: [4.899, 52.372]
+    })
 
-    return (
-        <>
-        <Head>
-            <title>Hello</title>
-            <link href="https://api.mapbox.com/mapbox-gl-js/v1.11.0/mapbox-gl.css" rel="stylesheet" />
-        </Head>
-        <div className={styles.map} ref={mapRef} />
-        </>
+    map.on('mouseenter', 'postcodes', function () {
+      map.getCanvas().style.cursor = 'pointer'
+    })
+
+    map.on('mouseleave', 'postcodes', function () {
+      map.getCanvas().style.cursor = ''
+    })
+
+    map.on('load', () => {
+      map.addLayer({
+        id: 'postcodes',
+        type: 'fill',
+        source: {
+          type: 'vector',
+          url: `mapbox://${postcodeId}`,
+        },
+        'source-layer': 'SDM730435-7u4d6a',
+        filter: [
+          'match',
+          ['get', 'POSTCODE'],
+          restrictedPostcodes,
+          true,
+          false,
+        ],
+        paint: {
+          // 'line-width': 1,
+          'fill-color': 'rgba(200, 100, 240, 0.4)',
+          'fill-outline-color': 'rgba(200, 100, 240, 1)',
+        },
+      })
+
+      // map.setFilter('postcodes', )
+    })
+
+    map.on('click', 'postcodes', function (e) {
+      new mapboxgl.Popup()
+        .setLngLat(e.lngLat)
+        .setHTML(`<p>Postcode: ${e.features[0].properties.POSTCODE}</p>`)
+        .addTo(map)
+    })
+
+    map.addControl(
+      new mapboxgl.GeolocateControl({
+        positionOptions: {
+          enableHighAccuracy: true,
+        },
+        trackUserLocation: true,
+      })
     )
+    map.addControl(new mapboxgl.FullscreenControl())
+    map.addControl(new mapboxgl.NavigationControl())
+
+    return () => {
+      map.remove()
+    }
+  }, [])
+
+  return (
+    <>
+      <Head>
+        <title>Lockdown Map</title>
+        <link
+          href="https://api.mapbox.com/mapbox-gl-js/v1.11.0/mapbox-gl.css"
+          rel="stylesheet"
+        />
+      </Head>
+      <div className={styles.map} ref={mapRef} />
+    </>
+  )
 }
 
 export default Map
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,15 +3,31 @@ import React from 'react'
 
 import Map from '../components/Map'
 
+import styles from '../styles/index.module.css'
+
 export const Home = (): JSX.Element => {
   return (
     <>
       <div>
-        <Map/>
+        <h1>Victoria Postcode Lockdown Map</h1>
+      </div>
+      <div className={styles.map}>
+        <Map />
+      </div>
+      <div>
+        <p>
+          Currently state 3 restrictions are in effect in the following
+          postcodes 3012, 3021, 3032, 3038, 3042, 3046, 3047, 3055, 3060 and
+          3064. This map is meant to be used as a visual aid representing the
+          offical data found on the{' '}
+          <a href="https://www.dhhs.vic.gov.au/restrictions-restricted-postcodes-covid-19">
+            website
+          </a>{' '}
+          for the Victorian Health and Human Services Department
+        </p>
       </div>
     </>
   )
 }
-
 
 export default Home
