779b6af43ec10c188188233e9ccb07933596a54f
Robert Koch <robekoc@amazon.com>
2020-07-07 11:34:47 +1000

    added goals for analytics

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,6 +4,7 @@ import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faGithub, faTwitter } from '@fortawesome/free-brands-svg-icons'
 import { faCoffee } from '@fortawesome/pro-duotone-svg-icons'
+import { trackGoal } from 'fathom-client'
 
 import styles from '../styles/footer.module.css'
 
@@ -16,6 +17,7 @@ const Footer = (): JSX.Element => {
           <a
             className={styles.link}
             href={'https://github.com/kochie/lockdown-map'}
+            onClick={() => trackGoal('AS0YU4N6', 0)}
           >
             <FontAwesomeIcon
               className={styles.icon}
@@ -23,14 +25,22 @@ const Footer = (): JSX.Element => {
               size={'lg'}
             />
           </a>
-          <a className={styles.link} href={'https://twitter.com/kochie'}>
+          <a
+            className={styles.link}
+            href={'https://twitter.com/kochie'}
+            onClick={() => trackGoal('04MKURQH', 0)}
+          >
             <FontAwesomeIcon
               className={styles.icon}
               icon={faTwitter}
               size={'lg'}
             />
           </a>
-          <a className={styles.link} href={'https://buymeacoff.ee/kochie'}>
+          <a
+            className={styles.link}
+            href={'https://buymeacoff.ee/kochie'}
+            onClick={() => trackGoal('NSGNK1ZO', 0)}
+          >
             <FontAwesomeIcon
               className={styles.icon}
               icon={faCoffee}
